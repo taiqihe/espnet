8b76c2fd79fd73f10bd660572d764868b9e88c6e
taiqihe <htq00007@gmail.com>
2022-10-14 17:53:27 -0400

    updated params

--- egs2/wav2gloss/asr1/run.sh
+++ egs2/wav2gloss/asr1/run.sh
@@ -9,26 +9,27 @@ train_set="train"
 train_dev="dev"
 test_set="test"
 
-asr_config=conf/train_asr_xlsr53_conformer.yaml
+asr_config=conf/train_asr_transformer.yaml
 inference_config=conf/decode_asr.yaml
 
 ./asr.sh \
     --local_data_opts "--stage 1" \
-    --stage 1 \
+    --stage 11 \
     --stop_stage 100 \
     --ngpu 1 \
     --nj 16 \
     --inference_nj 16 \
     --use_lm true \
     --token_type bpe \
-    --nbpe 600 \
+    --nbpe 1000 \
     --feats_type raw \
+    --audio_format wav \
     --asr_config "${asr_config}" \
     --inference_config "${inference_config}" \
     --train_set "${train_set}" \
     --valid_set "${train_dev}" \
     --test_sets "${test_set}" \
     --inference_asr_model valid.acc.best.pth \
-    --lm_train_text "data/${train_set}/text"  "$@" \
-    --expdir 'xlsr_ers_text'
-
+    --expdir 'jya_text' \
+    --lm_train_text "data/${train_set}/text"  "$@"
+    
