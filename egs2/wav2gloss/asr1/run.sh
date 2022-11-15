3010127cf9f4525c9bfdb9a3533f77800f71505f
taiqihe <taiqih@andrew.cmu.edu>
2022-11-15 11:50:07 -0500

    update asr config

--- egs2/wav2gloss/asr1/run.sh
+++ egs2/wav2gloss/asr1/run.sh
@@ -14,14 +14,13 @@ inference_config=conf/decode_asr.yaml
 
 ./asr.sh \
     --local_data_opts "--stage 1" \
-    --stage 11 \
+    --stage 1 \
     --stop_stage 100 \
     --ngpu 1 \
     --nj 16 \
     --inference_nj 16 \
     --use_lm true \
-    --token_type bpe \
-    --nbpe 1000 \
+    --token_type char \
     --feats_type raw \
     --audio_format wav \
     --asr_config "${asr_config}" \
@@ -30,6 +29,6 @@ inference_config=conf/decode_asr.yaml
     --valid_set "${train_dev}" \
     --test_sets "${test_set}" \
     --inference_asr_model valid.acc.best.pth \
-    --expdir 'jya_text' \
+    --expdir 'kamas_text' \
     --lm_train_text "data/${train_set}/text"  "$@"
     
